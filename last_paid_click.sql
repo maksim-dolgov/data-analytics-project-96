12e97b2bd7c15262011ea651a7ed1d560c6cf056
maksim-dolgov <dolgovm290605@gmail.com>
2025-11-17 15:46:21 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -11,5 +11,7 @@ select
     l.status_id
 from sessions as s left join sessions as s2 on s.visitor_id = s2.visitor_id
 left join leads as l on s.visitor_id = l.visitor_id
+where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
 group by 1, 3, 4, 5, 6, 7, 8, 9, 10
 order by 8 desc nulls last, 2, 3, 4, 5;
+
